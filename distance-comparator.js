815717c40862be790f6fc5d4ce22d584cd88e441
Volodymyr Sapsai <vsapsai@gmail.com>
2015-08-30 14:54:03 -0700

    Store maps not per module but per `initialize` invocation.

--- distance-comparator.js
+++ distance-comparator.js
@@ -15,77 +15,85 @@ var DistanceComparator = (function() {
         );
     }
 
-    var maps = [];
-    var circles = [];
-    var locationMarker;
+    var DistanceComparator = function() {
+        this.maps = [];
+        this.circles = [];
+        this.locationMarker = new google.maps.Marker({
+            icon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
+        });
+        this.isBoundsUpdateInProgress = false;
+    };
 
-    var exportObject = {};
-    exportObject.initialize = function(mapSettings, initialZoom) {
-          var i;
-          mapSettings.forEach(function(mapSetting, i) {
+    DistanceComparator.prototype.syncBoundsWithMap = function(mapIndex) {
+        if (this.isBoundsUpdateInProgress) {
+            return;
+        }
+        this.isBoundsUpdateInProgress = true;
+        var zoom = this.maps[mapIndex].getZoom();
+        var centerDiff = getLatLngDifference(this.mapSettings[mapIndex].center, this.maps[mapIndex].getCenter());
+        var i;
+        for (i = 0; i < this.maps.length; i++) {
+            if (i === mapIndex) {
+                continue;
+            }
+            this.maps[i].setZoom(zoom);
+            this.maps[i].setCenter(latLngByApplyingDifference(this.mapSettings[i].center, centerDiff));
+        }
+        this.isBoundsUpdateInProgress = false;
+    };
+
+    DistanceComparator.prototype.attach = function(mapSettings, initialZoom) {
+        var self = this;
+        self.mapSettings = mapSettings;
+        //var i;
+        mapSettings.forEach(function(mapSetting, i) {
             var map = new google.maps.Map(mapSettings[i].element, {
-              zoom: initialZoom,
-              center: mapSetting.center,
-              disableDoubleClickZoom: true
+                zoom: initialZoom,
+                center: mapSetting.center,
+                disableDoubleClickZoom: true
             });
-            maps.push(map);
+            self.maps.push(map);
             var circle = new google.maps.Circle({
-              map: map,
-              center: mapSetting.center,
-              fillOpacity: 0.0,
-              strokeColor: "#AAA",
-              strokeOpacity: 1.0,
-              strokeWeight: 2,
-              visible: false,
-              clickable: false
+                map: map,
+                center: mapSetting.center,
+                fillOpacity: 0.0,
+                strokeColor: "#AAA",
+                strokeOpacity: 1.0,
+                strokeWeight: 2,
+                visible: false,
+                clickable: false
             });
-            circles.push(circle);
-            google.maps.event.addDomListener(map, "bounds_changed", syncBoundsWithMap.bind(undefined, i));
+            self.circles.push(circle);
+            google.maps.event.addDomListener(map, "bounds_changed", self.syncBoundsWithMap.bind(self, i));
             google.maps.event.addDomListener(map, "dblclick", function(event) {
-              locationMarker.setMap(map);
-              locationMarker.setPosition(event.latLng);
+                self.locationMarker.setMap(map);
+                self.locationMarker.setPosition(event.latLng);
 
-              var radius = google.maps.geometry.spherical.computeDistanceBetween(mapSetting.center, event.latLng);
-              var i;
-              for (i = 0; i < circles.length; i++) {
-                circles[i].setRadius(radius);
-                circles[i].setVisible(true);
-              }
+                var radius = google.maps.geometry.spherical.computeDistanceBetween(mapSetting.center, event.latLng);
+                var i;
+                for (i = 0; i < self.circles.length; i++) {
+                    self.circles[i].setRadius(radius);
+                    self.circles[i].setVisible(true);
+                }
             });
 
             var marker = new google.maps.Marker({
-              position: mapSetting.center,
-              map: map,
-              draggable: true
+                position: mapSetting.center,
+                map: map,
+                draggable: true
             });
             google.maps.event.addDomListener(marker, "dragend", function() {
-              mapSettings[i].center = marker.getPosition();
-              circle.setCenter(mapSettings[i].center);
-              syncBoundsWithMap(i);
+                self.mapSettings[i].center = marker.getPosition();
+                circle.setCenter(self.mapSettings[i].center);
+                self.syncBoundsWithMap(i);
             });
-          });
-          locationMarker = new google.maps.Marker({
-            icon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
-          });
+        });
+    };
 
-          var isBoundsUpdateInProgress = false;
-          function syncBoundsWithMap(mapIndex) {
-            if (isBoundsUpdateInProgress) {
-              return;
-            }
-            isBoundsUpdateInProgress = true;
-            var zoom = maps[mapIndex].getZoom();
-            var centerDiff = getLatLngDifference(mapSettings[mapIndex].center, maps[mapIndex].getCenter());
-            var i;
-            for (i = 0; i < maps.length; i++) {
-              if (i === mapIndex) {
-                continue;
-              }
-              maps[i].setZoom(zoom);
-              maps[i].setCenter(latLngByApplyingDifference(mapSettings[i].center, centerDiff));
-            }
-            isBoundsUpdateInProgress = false;
-          }
+    var exportObject = {};
+    exportObject.initialize = function(mapSettings, initialZoom) {
+        var distanceComparator = new DistanceComparator();
+        distanceComparator.attach(mapSettings, initialZoom);
     };
 
     return exportObject;
