473bcc69ec79228dd10e06102412c07bb6f92de9
Volodymyr Sapsai <vsapsai@gmail.com>
2015-08-30 15:37:27 -0700

    Hide low-level map manipulation inside MapView.

--- distance-comparator.js
+++ distance-comparator.js
@@ -15,9 +15,76 @@ var DistanceComparator = (function() {
         );
     }
 
+    /**
+     * Represents a single map.
+     */
+    var MapView = function(mapConfig, zoom) {
+        this.referencePoint = mapConfig.center;
+        this.map = new google.maps.Map(mapConfig.element, {
+            zoom: zoom,
+            center: mapConfig.center,
+            disableDoubleClickZoom: true  // We use double click for a different purpose.
+        });
+        this.circle = new google.maps.Circle({
+            map: this.map,
+            center: mapConfig.center,
+            fillOpacity: 0.0,
+            strokeColor: "#AAA",
+            strokeOpacity: 1.0,
+            strokeWeight: 2,
+            visible: false,
+            clickable: false
+        });
+        var marker = new google.maps.Marker({
+            position: mapConfig.center,
+            map: this.map,
+            draggable: true
+        });
+        var self = this;
+        google.maps.event.addDomListener(this.map, "bounds_changed", function() {
+            self.delegate.mapDidMove(self);
+        });
+        google.maps.event.addDomListener(this.map, "dblclick", function(event) {
+            self.delegate.mapDidDoubleClick(self, event);
+        });
+        google.maps.event.addDomListener(marker, "dragend", function() {
+            self.referencePoint = marker.getPosition();
+            self.circle.setCenter(self.referencePoint);
+            self.delegate.mapDidMove(self);
+        });
+    };
+
+    MapView.prototype.getMap = function() {
+        return this.map;
+    };
+
+    MapView.prototype.getZoom = function() {
+        return this.map.getZoom();
+    };
+
+    MapView.prototype.setZoom = function(zoom) {
+        this.map.setZoom(zoom);
+    };
+
+    MapView.prototype.getCenterOffset = function() {
+        return getLatLngDifference(this.referencePoint, this.map.getCenter());
+    };
+
+    MapView.prototype.setCenterOffset = function(offset) {
+        this.map.setCenter(latLngByApplyingDifference(this.referencePoint, offset));
+    };
+
+    MapView.prototype.getDistanceToReferencePoint = function(point) {
+        return google.maps.geometry.spherical.computeDistanceBetween(this.referencePoint, point);
+    };
+
+    MapView.prototype.setCircleRadius = function(radius) {
+        this.circle.setRadius(radius);
+        this.circle.setVisible(true);
+    };
+
     var DistanceComparator = function() {
         this.maps = [];
-        this.circles = [];
         this.locationMarker = new google.maps.Marker({
             icon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
         });
@@ -30,63 +97,40 @@ var DistanceComparator = (function() {
         }
         this.isBoundsUpdateInProgress = true;
         var zoom = this.maps[mapIndex].getZoom();
-        var centerDiff = getLatLngDifference(this.mapSettings[mapIndex].center, this.maps[mapIndex].getCenter());
+        var centerOffset = this.maps[mapIndex].getCenterOffset();
         var i;
         for (i = 0; i < this.maps.length; i++) {
             if (i === mapIndex) {
                 continue;
             }
             this.maps[i].setZoom(zoom);
-            this.maps[i].setCenter(latLngByApplyingDifference(this.mapSettings[i].center, centerDiff));
+            this.maps[i].setCenterOffset(centerOffset);
         }
         this.isBoundsUpdateInProgress = false;
     };
 
+    DistanceComparator.prototype.mapDidMove = function(mapView) {
+        this.syncBoundsWithMap(mapView.tag);
+    };
+
+    DistanceComparator.prototype.mapDidDoubleClick = function(mapView, event) {
+        this.locationMarker.setMap(mapView.getMap());
+        this.locationMarker.setPosition(event.latLng);
+
+        var radius = mapView.getDistanceToReferencePoint(event.latLng);
+        var i;
+        for (i = 0; i < this.maps.length; i++) {
+            this.maps[i].setCircleRadius(radius);
+        }
+    };
+
     DistanceComparator.prototype.attach = function(mapSettings, initialZoom) {
         var self = this;
-        self.mapSettings = mapSettings;
-        //var i;
         mapSettings.forEach(function(mapSetting, i) {
-            var map = new google.maps.Map(mapSettings[i].element, {
-                zoom: initialZoom,
-                center: mapSetting.center,
-                disableDoubleClickZoom: true
-            });
+            var map = new MapView(mapSetting, initialZoom);
+            map.delegate = self;
+            map.tag = i;
             self.maps.push(map);
-            var circle = new google.maps.Circle({
-                map: map,
-                center: mapSetting.center,
-                fillOpacity: 0.0,
-                strokeColor: "#AAA",
-                strokeOpacity: 1.0,
-                strokeWeight: 2,
-                visible: false,
-                clickable: false
-            });
-            self.circles.push(circle);
-            google.maps.event.addDomListener(map, "bounds_changed", self.syncBoundsWithMap.bind(self, i));
-            google.maps.event.addDomListener(map, "dblclick", function(event) {
-                self.locationMarker.setMap(map);
-                self.locationMarker.setPosition(event.latLng);
-
-                var radius = google.maps.geometry.spherical.computeDistanceBetween(mapSetting.center, event.latLng);
-                var i;
-                for (i = 0; i < self.circles.length; i++) {
-                    self.circles[i].setRadius(radius);
-                    self.circles[i].setVisible(true);
-                }
-            });
-
-            var marker = new google.maps.Marker({
-                position: mapSetting.center,
-                map: map,
-                draggable: true
-            });
-            google.maps.event.addDomListener(marker, "dragend", function() {
-                self.mapSettings[i].center = marker.getPosition();
-                circle.setCenter(self.mapSettings[i].center);
-                self.syncBoundsWithMap(i);
-            });
         });
     };
 
